ebb0b5462bef4104f41d66eea921b8a564a4071d
vhraposo <thevhup@gmail.com>
2023-10-24 01:04:15 -0300

    build: add styled components dependency

--- src/pages/Details.jsx
+++
@@ -1,6 +0,0 @@
-export function Details(){
-  
-  return(
-    <h1>Hello World!</h1>
-  )
-}
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles"
+
+
+export function Details(){
+  
+  return(
+    <Container>
+      <h1>Hello World!</h1>
+    </Container>
+  )
+}
