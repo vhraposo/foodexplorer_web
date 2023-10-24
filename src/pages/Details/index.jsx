59abba6c388b2c5495dcd9a616ea3ef34b457032
vhraposo <thevhup@gmail.com>
2023-10-24 19:19:47 -0300

    feat: create header component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,13 @@
 import { Container } from "./styles"
 
+import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
 
 export function Details(){
   
   return(
     <Container>
-      <h1>Hello World!</h1>
-
+      <Header />
       <Button title="incluir ∙ R$ 25,00" loading />
     </Container>
   )
