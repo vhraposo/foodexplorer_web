6566b9f0b9ced45780b15c6d2b2cb9ef930a9700
vhraposo <thevhup@gmail.com>
2023-10-24 09:57:36 -0300

    style: add colors variable

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Details } from './pages/Details'
+import { ThemeProvider } from 'styled-components' 
+
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+        <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
