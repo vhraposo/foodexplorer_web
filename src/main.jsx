23e35f0bfcb81cc4135a4439212a5317a5dd04a6
vhraposo <thevhup@gmail.com>
2023-10-24 00:54:15 -0300

    refactor: remove unnecessary archives

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Details } from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Details />
   </React.StrictMode>,
 )
---
+++ src/pages/Details.jsx
@@ -0,0 +1,6 @@
+export function Details(){
+  
+  return(
+    <h1>Hello World!</h1>
+  )
+}
