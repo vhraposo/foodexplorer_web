1229d984b81fb3e4722ab834e9f82218cfc5f865
vhraposo <thevhup@gmail.com>
2023-10-24 22:10:48 -0300

    feat: create header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,23 @@
-import { Container } from "./styles"
+import { PiSignOut } from 'react-icons/pi'
+
+
+import { Container, Logout } from "./styles"
+import logo from "../../assets/logo.svg"
 
 export function Header(){
     return(
         <Container>
-            
+                
+                <div>
+                    <img src={logo} alt="Logo" />
+                    <span>food explorer</span>
+                </div>
+
+    
+
+                <Logout>
+                    <PiSignOut />
+                </Logout>
         </Container>
     )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,20 +2,55 @@ import styled from "styled-components"
 
 export const Container = styled.header`
     grid-area: header;
+   
+
 
     height: 10.5rem;
     width: 100%;
 
+    background-color: ${({ theme }) => theme.COLORS.DARKBLUE_600};;
     border-bottom-width: 1px;
     border-bottom-style: solid;
-    border-bottom-color: ${({ theme }) => theme.COLORS.LIGHT_300};
+    border-bottom-color: ${({ theme }) => theme.COLORS.DARKBLUE_600};
 
     display: flex;
     justify-content: space-between;
 
     padding: 0 8rem;
 
-    background-color: red;
+    > div{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        gap: 1rem;
+
+        >img{
+            width: 3rem;
+            height: 3rem;
+        }
+
+        > span{
+            text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+            font-family: Roboto;
+            font-size: 2.4rem;
+            font-style: normal;
+            font-weight: 700;
+            line-height: normal;
+        }
+    }
+`
+
+export const Logout = styled.button`
+    border: none;
+    background: none;
+
+    > svg{
+        width: 3.2rem;
+        height: 3.2rem;
+        flex-shrink: 0;
+    }
 
 `
 
