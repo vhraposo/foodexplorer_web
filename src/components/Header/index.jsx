f2d9ec30e1cf3a3af613d7ff94a056073c8c6723
vhraposo <thevhup@gmail.com>
2023-10-29 01:19:50 -0300

    feat: add menu-mobile at header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,48 @@
-import { PiSignOut } from 'react-icons/pi'
-import { GiHamburgerMenu } from 'react-icons/gi'
+import { useState } from 'react';
+import { PiSignOut } from 'react-icons/pi';
+import { GiHamburgerMenu, GiKnifeFork } from 'react-icons/gi';
+import { Container, Logout } from './styles';
+import logo from '../../assets/logo.svg';
 
+export function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+  
 
-import { Container, Logout, Menu } from "./styles"
-import logo from "../../assets/logo.svg"
+  return (
+    <Container $menuIsOpen={isMenuOpen}>
+      <button onClick={toggleMenu}>
+        <GiHamburgerMenu />
+      </button>
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+        <span>food explorer</span>
+      </div>
 
-export function Header(){
-    return(
-        <Container>
-                
-                <Menu className='hamburguer-menu'>
-                    <GiHamburgerMenu/>
-                </Menu>
-                
-                <div className='logo'>
-                    <img src={logo} alt="Logo" />
-                    <span>food explorer</span>
-                </div>
-                <Logout>
-                    <PiSignOut />
-                </Logout>
-        </Container>
-    )
+      <div className={`menu-open`}>
+        <div className='heading-menu' onClick={toggleMenu}>
+          <GiKnifeFork />
+          <h2>Menu</h2>
+        </div>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">Sobre</a>
+          </li>
+          <li>
+            <a href="/contact">Contato</a>
+          </li>
+        </ul>
+      </div>
+
+      <Logout>
+        <PiSignOut />
+      </Logout>
+    </Container>
+  );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,32 +1,128 @@
-import styled from "styled-components"
+import styled, { keyframes, css } from "styled-components";
+
+const slideIn = keyframes`
+  from {
+    width: 0;
+    opacity: 0;
+  }
+  to {
+    width: 100%;
+    opacity: 1;
+  }
+`
+
+const slideOut = keyframes`
+  from {
+    width: 100%;
+    opacity: 1;
+  }
+  to {
+    width: 0;
+    opacity: 0;
+  }
+`
+
+
+
 
 export const Container = styled.header`
     grid-area: header;
-   
+    padding: 1.6rem;
     height: 11.4rem;
     width: 100%;
 
-    background-color: ${({ theme }) => theme.COLORS.DARKBLUE_600};;
+    background-color: ${({ theme }) => theme.COLORS.DARKBLUE_600};
     border-bottom-width: 1px;
     border-bottom-style: solid;
     border-bottom-color: ${({ theme }) => theme.COLORS.DARKBLUE_600};
 
     display: flex;
     justify-content: space-between;
+    align-items: center;
 
+    button{
+        background-color: transparent;
+        border: none;
+        width: fit-content;
+        min-height: fit-content;       
+    }
+    button svg{
+        width: 3.2rem;
+        height: 3.2rem;    
+    }
+    
+    .menu-open{
+        ${({ $menuIsOpen }) =>
+      $menuIsOpen
+        ? css`
+            animation: ${slideIn} 0.5s ease-in-out;
+            display: block;
+          `
+        : css`
+            animation: ${slideOut} 0.5s ease-in-out;
+            
+          `}
+
+        width: 100%;
+        height: 100%;
+        background-color: ${({ theme }) => theme.COLORS.DARKBLUE_600};
+        padding: 2.4rem 3.2rem;
+        position: absolute;
+        z-index: 9999;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        margin: unset;
+        transition: all 5.3s ease-in-out;
+        .heading-menu{
+            display: flex;
+            align-items: center;
+            gap: 1.6rem;
+
+            height: 11.4rem;
+            width: 100%;
+
+            >svg{
+            width: 3.2rem;
+            height: 3.2rem;
+            cursor: pointer;
+            }
+            >h2{
+                font-size: 2.1rem;
+                font-style: normal;
+                font-weight: 400;
+
+            }
+        }
+
+        ul{
+            li{
+                font-size: 2.4rem;
+
+                display: flex;
+                align-items: center;
+                
+                margin-top: 2rem;
+                border-bottom: 1px solid var(--dark-dark-1000, #192227);
   
-
-    > div{
+            a{
+                color: ${({ theme }) => theme.COLORS.LIGHT_300};
+                text-decoration: none;
+            }
+            }
+        }
+    }
+    
+    .logo{
         display: flex;
         justify-content: center;
         align-items: center;
 
         gap: 1rem;
-        margin-top: 2rem;
+        
         >img{
             width: 3rem;
-            height: 3rem;
-            
+            height: 3rem;          
         }
 
         > span{
@@ -39,41 +135,22 @@ export const Container = styled.header`
             line-height: normal;
         }
     }
+    
+
 `
 
 export const Logout = styled.button`
     border: none;
     background: none;
-    margin-top: 2rem;
     display: flex;
     align-items: center;
 
     > svg{
         width: 3.2rem;
         height: 3.2rem;
-        flex-shrink: 0;
-
-        margin-right: 1.6rem;
     }
 
 `
 
-export const Menu = styled.button`
-    border: none;
-    background: none;
-
-    margin-top: 2rem;
-    display: flex;
-    align-items: center;
-    
-
-    > svg{
-        width: 3.4rem;
-        height: 2.5rem;
-        margin-left: 1.6rem;
-        
-    }
-
-`
 
 
