f040917cea4bc6e6f39db363e5796456abf32202
vhraposo <thevhup@gmail.com>
2023-10-31 01:10:32 -0300

    feat: create signOut Page

---
+++ src/pages/SignOut/index.jsx
@@ -0,0 +1,45 @@
+import { BsPerson } from 'react-icons/bs'
+import { MdOutlineMailLock } from 'react-icons/md'
+import { TfiLock } from 'react-icons/tfi'
+import polygon from '../../assets/polygon.svg'
+import { Button } from '../../components/Button'
+import { Input } from '../../components/Input'
+
+import { Container, Form } from './styles'
+
+export function SignOut() {
+  return (
+    <Container>
+      <div className="image-logo">
+        <img src={polygon} alt="#" />
+        <h1>food explorer</h1>
+      </div>
+      <Form>
+        <h2 className="hidden">Faça Login</h2>
+        <Input
+          placeholder={'Exemplo: Victor Raposo'}
+          label={'Seu nome'}
+          icon={BsPerson}
+        ></Input>
+        <Input
+          placeholder={'Exemplo: exemplo@exemplo.com.br'}
+          label={'Email'}
+          icon={MdOutlineMailLock}
+        ></Input>
+
+        <Input
+          placeholder={'No mínimo 6 caracteres'}
+          label={'Senha'}
+          icon={TfiLock}
+          type={'password'}
+        ></Input>
+        <Button title="Entrar" />
+
+        <a href="">
+          <link href="" />
+          CRIAR CONTA
+        </a>
+      </Form>
+    </Container>
+  )
+}
