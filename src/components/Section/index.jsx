240b04d512a553a1cac0356442e023eca88cb1a2
vhraposo <thevhup@gmail.com>
2023-12-04 15:33:26 -0300

    refactor: Section component to fetch and display dish data

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react'
 import { PiArrowArcLeftDuotone, PiReceiptLight } from 'react-icons/pi'
 import { TiMinus, TiPlus } from 'react-icons/ti'
+import { useParams } from 'react-router-dom'
 import { Button } from '../../components/Button'
+import { api } from '../../services/api'
 import { Container } from './styles'
 
-import dish from '../../assets/dish.png'
-
 export function Section({ title, children }) {
+  const params = useParams()
+  const [data, setData] = useState(null)
+  const { name, description, price, image, ingredients } = data ?? {}
+  const formattedPrice = `R$ ${price ? price.toFixed(2) : '0.00'}`
+
+  useEffect(() => {
+    async function fetchDish() {
+      const response = await api.get(`/dishes/${params.id}`)
+      setData(response.data)
+    }
+
+    fetchDish()
+  }, [])
   return (
     <Container>
       <div className="backdiv" title="voltar">
@@ -14,23 +28,17 @@ export function Section({ title, children }) {
       </div>
 
       <div className="wrap">
-        <img src={dish} alt="" />
+        <img src={image} alt={`imagem de '${name}'`} />
         <div className="dish-info">
-          <h2 className="section-title">{title}</h2>
+          <h2 className="section-title">{name}</h2>
           {children}
 
-          <p className="section-description">
-            Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. O
-            pão naan dá um toque especial.
-          </p>
+          <p className="section-description">{description}</p>
           <ul className="section-ingredients">
-            <li>Ingrediente</li>
-            <li>Ingrediente</li>
-            <li>Ingrediente</li>
-            <li>Ingrediente</li>
-            <li>Ingrediente</li>
-            <li>Ingrediente</li>
-            <li>Ingrediente</li>
+            {ingredients &&
+              ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient.name}</li>
+              ))}
           </ul>
 
           <div className="inputstepper">
@@ -39,8 +47,7 @@ export function Section({ title, children }) {
               <span id="count">1</span>
               <TiPlus className="cursor-hability-pointer" />
             </div>
-
-            <Button icon={PiReceiptLight} title="pedir ∙ R$ 25,00" />
+            <Button icon={PiReceiptLight} title={`pedir ∙ ${formattedPrice}`} />
           </div>
         </div>
       </div>
