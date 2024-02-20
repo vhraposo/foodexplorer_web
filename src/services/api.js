aac80bf5f0b8688a5eb451eb022d3ee2f66eb368
vhraposo <thevhup@gmail.com>
2024-02-19 22:56:16 -0300

    deploy: update API base URL to koyeb

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
   // baseURL: 'http://localhost:3334',
-  baseURL: 'https://food-explorer-c78s.onrender.com',
+  baseURL: 'https://food-explorer-raposo.koyeb.app',
 })
